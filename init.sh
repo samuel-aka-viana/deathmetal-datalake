abb7cfa798d7f1340cb802ac07b60f6f07c0f6c7
samuel viana <samuelviana2626@gmail.com>
2025-05-13 15:03:30 -0400

    branch do polars

--- init.sh
+++
@@ -1,8 +0,0 @@
-curl -sfL https://get.k3s.io | sh -
-
-kubectl apply -k cluster/
-helm repo add nessie-helm https://charts.projectnessie.org
-helm repo add trino https://trinodb.github.io/charts
-helm install -n lakehouse nessie nessie-helm/nessie
-helm install -n lakehouse -f charts/minio/values.yaml minio bitnami/minio
-helm install -n lakehouse -f charts/trino/values.yaml trino trino/trino
